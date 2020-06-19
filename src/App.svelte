00066a4080ce0edc0f3f4cb3e3725c267eed4cc4
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-06-19 11:28:49 +0200

    Wip: welcome screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,24 @@
 <script>
+  import Welcome from "./screens/Welcomes.svelte";
 
+  let state = "welcome"; // or "playing"
 </script>
 
 <style>
-
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 800px;
+    margin: 0 auto;
+    height: 100%;
+    justify-content: center;
+  }
 </style>
 
-<main />
+<main>
+  {#if state === 'welcome'}
+    <Welcome />
+  {:else if state === 'playing'}
+    <p>Playing Screen goes here</p>
+  {/if}
+</main>
