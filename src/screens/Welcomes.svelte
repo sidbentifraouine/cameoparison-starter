52dcdc6e61206caed0a1e662a555746a49a5bed9
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-06-19 17:19:54 +0200

    adjust welcome style

--- src/screens/Welcomes.svelte
+++ src/screens/Welcomes.svelte
@@ -51,6 +51,19 @@
     max-width: 26em;
     margin: 0 auto;
   }
+
+  button {
+    padding: 0.5em 1em;
+    margin: 0 0.2em 0.2em;
+  }
+
+  @media (miin-width: 640px) {
+    button {
+      display: inline;
+      margin: 0 0.2em 0.4em 0.2em;
+      width: auto;
+    }
+  }
 </style>
 
 <header>
