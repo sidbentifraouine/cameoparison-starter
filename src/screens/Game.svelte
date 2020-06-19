c7b7bc7a616897a28c3537f3331a702d8c6a0915
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-06-19 17:29:52 +0200

    wip Game images animate

--- src/screens/Game.svelte
+++ src/screens/Game.svelte
@@ -1,5 +1,7 @@
 <script>
   import { createEventDispatcher } from "svelte";
+  import { fly } from "svelte/transition";
+  import * as eases from "svelte/easing";
   import Card from "../components/Card.svelte";
   import { sleep, pick_random, load_image } from "../utils";
 
@@ -25,6 +27,7 @@
   let i = 0;
   let last_result;
   let done = false;
+  let ready = true;
 
   $: score = results.filter((x) => x === "right").length;
 
@@ -158,9 +161,14 @@
       <p>{pick_message(score / results.length)}</p>
       <button on:click={() => dispatch('restart')}>Back to main screen</button>
     </div>
-  {:else}
+  {:else if ready}
     {#await promises[i] then [a, b]}
-      <div class="game">
+      <div
+        class="game"
+        in:fly={{ duration: 200, y: -20 }}
+        out:fly={{ duration: 200, y: 20 }}
+        on:outrostart={() => (ready = false)}
+        on:outroend={() => (ready = true)}>
         <div class="card-container">
           <Card
             celeb={a}
